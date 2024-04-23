631f8c8dbebbaf2a63ccee5f51bc3db794bc0c00
webdev-bfunk <webdev.bfunk@gmail.com>
2024-04-22 22:30:49 -0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/57f5ca0c-fa0e-4591-a976-6721a3793012-qbhshb.PNG",
+  "https://utfs.io/f/8e6a35bb-a74f-4570-8179-7856b9ce1e5e-ur8vv5.png",
+  "https://utfs.io/f/05dfcfec-8a3b-4511-af0e-0a71adb75391-oxcsfx.png",
+  "https://utfs.io/f/8177d79e-ea61-4924-ba41-e27f013e1bee-dmhted.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
+export default function HomePage() {
+  return (
+    <main className="">
+      <div className="flex flex-wrap justify-center gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48 p-4">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
