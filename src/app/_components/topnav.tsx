1147bf5573c80959bdbfbdff3ba8fb4196bc9ab5
webdev-bfunk <webdev.bfunk@gmail.com>
2024-04-23 20:49:08 -0400

    Authentication is working.

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold md:px-16">
+      <div>T3Gallery</div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,19 @@
 import { db } from "~/server/db";
 
-const mockUrls = [
-  "https://utfs.io/f/57f5ca0c-fa0e-4591-a976-6721a3793012-qbhshb.PNG",
-  "https://utfs.io/f/8e6a35bb-a74f-4570-8179-7856b9ce1e5e-ur8vv5.png",
-  "https://utfs.io/f/05dfcfec-8a3b-4511-af0e-0a71adb75391-oxcsfx.png",
-  "https://utfs.io/f/8177d79e-ea61-4924-ba41-e27f013e1bee-dmhted.png",
-];
+export const dynamic = "force-dynamic";
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-``;
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap justify-center gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {[...images, ...images, ...images].map((image, index) => (
           <div key={image.id + "-" + index} className="w-48 p-4">
             <img src={image.url} alt="image" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
