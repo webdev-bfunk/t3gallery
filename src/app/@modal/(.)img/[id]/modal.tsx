676d4ca502bbf43ec2b5670d06308aef2c15f226
webdev-bfunk <webdev.bfunk@gmail.com>
2024-04-30 15:14:28 -0400

    fixed modal layout

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,7 +21,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="m-0 h-screen w-screen bg-zinc-900/70 text-white"
+      className="m-0 h-screen w-screen bg-zinc-900/90 text-white"
       onClose={onDismiss}
     >
       {children}
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -11,20 +11,22 @@ export async function FullPageImageView(props: { photoId: string }) {
   const userInfo = await clerkClient.users.getUser(image.userId);
 
   return (
-    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
-      <div className="flex-shrink flex-grow">
+    <div className="flex h-full w-full min-w-0 items-center justify-center text-white">
+      <div className="flex flex-grow justify-center">
         <img src={image.url} className="object-contain" alt={image.name} />
       </div>
-      <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
-        <div className="border-b p-2 text-center text-xl">{image.name}</div>
+      <div className="w-75 flex h-full flex-shrink-0 flex-col border-l">
+        <div className="border-b px-8 pt-10 text-center text-3xl font-bold">
+          {image.name}
+        </div>
 
         <div className="p-2">
-          <div>Uploaded By:</div>
+          <div className="text-2xl font-bold">Uploaded By:</div>
           <div>{userInfo.fullName}</div>
         </div>
 
         <div className="p-2">
-          <div>Created On:</div>
+          <div className="text-2xl font-bold ">Created On:</div>
           <div>{image.createdAt.toLocaleDateString()}</div>
         </div>
 
